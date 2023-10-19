82d44ba768faa58983223282c5eeddc8f6a2e461
Prathmesh2498 <prathu10@gmail.com>
2023-10-19 15:14:30 -0400

    Changed login user session management to follow pep8

--- src/controller/user_controller.py
+++ src/controller/user_controller.py
@@ -1,11 +1,13 @@
 from src.models.user_model import user_model
 user = user_model()
 
+
 def login_control(data):
     loginStatus = user.login(data)
     if loginStatus:
         return True
     return False
+
 
 def get_loggedIn_User(username):
     userDetails = user.get_loggedIn_User(username)
